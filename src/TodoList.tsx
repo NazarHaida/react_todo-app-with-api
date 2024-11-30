db5a45d02caf1fb0e5fd99d75de7fd3ed56b751a
NazarHaida <nazar.haida.knm.2020@lpnu.ua>
2024-11-30 03:52:51 +0200

    add task solution

---
+++ src/TodoList.tsx
@@ -0,0 +1,42 @@
+import { Todo } from './types/Todo';
+import { TodoItem } from './TodoItem';
+
+type Props = {
+  todos: Todo[];
+};
+
+export const TodoList: React.FC<Props> = ({
+  deletingTodoId,
+  todos,
+  updateTodos,
+  setTodos,
+  setError,
+  updateStatus,
+  handleDelete,
+  handleStatus,
+  updatingStatusId,
+  updatingStatusIds,
+  isSubmitting,
+}) => {
+  return (
+    <section className="todoapp__main" data-cy="TodoList">
+      {todos.map(todo => (
+        <TodoItem
+          isDeleting={deletingTodoId === todo.id}
+          todo={todo}
+          todos={todos}
+          updateTodos={updateTodos}
+          setTodos={setTodos}
+          setError={setError}
+          key={todo.id}
+          updateStatus={updateStatus}
+          handleDelete={handleDelete}
+          handleStatus={handleStatus}
+          updatingStatusIds={updatingStatusIds}
+          isUpdatingStatus={updatingStatusId === todo.id}
+          isSubmitting={isSubmitting}
+        />
+      ))}
+    </section>
+  );
+};
